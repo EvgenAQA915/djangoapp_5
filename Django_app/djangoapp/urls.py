a06fe01eb789c0c96c1466b2d9659068c8ca406f
unknown <gitevgenaqa911@mail.ru>
2024-06-13 18:44:54 +0500

    cbv

--- Django_app/djangoapp/urls.py
+++ Django_app/djangoapp/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from djangoapp.views import index, indexItem, add_item, update_item, delete_item
+from djangoapp.views import add_item, update_item, delete_item, ProductListView, ProductDetailView
 
 app_name = "djangoapp"
 
 urlpatterns = [
     # http://127.0.0.1:8000/djangoapp/
-    path("", index, name="index"),
-    path("<int:my_id>/", indexItem, name="detail"),
+    # path("", index, name="index"),
+    path("", ProductListView.as_view(), name="index"),
+    path("<int:pk>/", ProductDetailView.as_view(), name="detail"),
     # http://127.0.0.1:8000/djangoapp/
     path("additem/", add_item, name="add_item"),
     path("updateitem/<int:my_id>/", update_item, name="update_item"),
